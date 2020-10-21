05a414b464f4a1a196e90b3c74981ff13148a532
Luiz Doreto <luiz.doreto@icloud.com>
2020-10-20 23:08:49 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Luiz Doreto on 20/10/20.
+//  Copyright © 2020 Doreto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
